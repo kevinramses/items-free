f6c298468c13843913b46079e3ba972e48729c3a
kevinramses <homerinjuan@hotmail.com>
2022-08-05 15:29:09 -0500

    depliege6

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,4 +15,33 @@ router.get('/', async (req, res) => {
 	}
 });
 
+router.get('/home', async (req, res) => {
+	if (req.user) {
+		var user = req.user;
+		res.render ('home'  ,  {
+			user:  user, 
+		});
+	} else {
+		res.render('home', {
+			user:  "",
+			saldo:""
+	 })
+	}
+});
+
+router.get('/market', async (req, res) => {
+	if (req.user) {
+		var user = req.user;
+		res.render ('market'  ,  {
+			user:  user, 
+		});
+	} else {
+		res.render('market', {
+			user:  "",
+			saldo:""
+	 })
+	}
+});
+
+
 module.exports = router;
